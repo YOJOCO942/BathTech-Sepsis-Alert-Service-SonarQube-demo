e2064899e0c3743cc63d41f351cd28ccb8aa7498
有可 <joco722765@gmail.com>
2026-04-13 18:19:06 +0100

    v5 test

---
+++ src/main/java/com/bathtech/sepsis/SepsisAlertApplication.java
@@ -0,0 +1,12 @@
+package com.bathtech.sepsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepsisAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepsisAlertApplication.class, args);
+    }
+}
